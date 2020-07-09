25d104743b13c78fd5c165488619dae1bde3b7b7
Frizlab <fload@me.com>
2020-07-09 13:41:06 +0200

    Add specific iOS 8 compatibility for the project

--- Package.swift
+++ Package.swift
@@ -6,6 +6,12 @@ import PackageDescription
 
 let package = Package(
 	name: "DummyLinuxOSLog",
+	platforms: [
+		.macOS(.v10_10),
+		.iOS(.v8),
+		.tvOS(.v9),
+		.watchOS(.v2)
+	],
 	products: [
 		.library(name: "DummyLinuxOSLog", targets: ["DummyLinuxOSLog"]),
 	],
